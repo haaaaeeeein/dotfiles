9598fe7c7bad83838d9218a74b61490b7c4f6afc
Haein Lee <lhi0729@kaist.ac.kr>
2022-03-13 15:34:35 +0900

    fix command + script for jellybeans theme

--- install.sh
+++ install.sh
@@ -2,13 +2,16 @@
 BASEDIR=$(dirname $0)
 
 # vim
-cp -u $BASEDIR/.vimrc ~/.vimrc
+cp $BASEDIR/.vimrc ~/.vimrc
 if ! [ -e ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
+mkdir -p ~/.vim/colors
+cd ~/.vim/colors
+curl -O https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
 vim +PluginInstall +qall
 
 # tmux
-cp -u $BASEDIR/.tmux.conf ~/.tmux.conf
+cp $BASEDIR/.tmux.conf ~/.tmux.conf
 tmux source-file ~/.tmux.conf
 
