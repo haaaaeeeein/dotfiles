7e6ec602cb757a82be60f7ea5c5eef2c9f8fe0ea
Haein Lee <lhi0729@kaist.ac.kr>
2022-07-15 03:20:29 +0900

    Fix indentation, Use symlink instead of copy

--- .vimrc
+++ .vimrc
@@ -89,3 +89,6 @@ set undofile
 set undodir=$HOME/.vim/undo
 set undolevels=1000
 set undoreload=10000
+
+set paste
+set expandtab smartindent autoindent
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 HERE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # vim
-cp $HERE/.vimrc ~/.vimrc
+ln -s $HERE/.vimrc ~/.vimrc
 if ! [ -e ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
